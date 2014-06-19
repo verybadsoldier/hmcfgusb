b5850466f0afcc1ed8af7c920d8c02b24e02c404
JSurf <jsurf@gmx.de>
2014-06-19 13:53:34 +0200

    Starting to add debian build stuff

---
+++ debian/hmcfgusb.default.ex
@@ -0,0 +1,10 @@
+# Defaults for hmcfgusb initscript
+# sourced by /etc/init.d/hmcfgusb
+# installed at /etc/default/hmcfgusb by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
